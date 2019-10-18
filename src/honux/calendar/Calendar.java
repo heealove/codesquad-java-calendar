c24bebde7729369cfa52c55b3579bfa5690dbefc
s6569 <s6569@DESKTOP-015I7KE>
2019-10-18 16:07:54 +0900

    Implement find days of the month

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,14 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt();
+		
+		int maxDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d까지 있습니다.", month, maxDays[month-1]);
+		scanner.close();
 	}
 
 }
